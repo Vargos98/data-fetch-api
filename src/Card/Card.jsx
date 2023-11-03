df4981a6798aec982b91cf56a34f459320f0de9d
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-11-03 10:12:17 +0530

    Added background to card and implemented FlexBox

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -3,23 +3,66 @@ import Back from './back.jpg';
 import './Card.css';
 
 const Card = () => {
- const [click, handleClick] = useState("");
+  const [click, handleClick] = useState("");
 
 
 
   return (
-    <div className='box'>
-        
-            <img className='image'src={Back} alt="background" />
-        
+
+    <div className="outer-box">
+      <div className='box'>
+        <img className='image' src={Back} alt="background" />
         <div className='desc-box'>
-            <h3 className='title'>Albert hall</h3>
-            <div className='underline'></div>
-            <p className='desc'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, delectus?</p>
+          <h3 className='title'>Albert hall</h3>
+          <div className='underline'></div>
+          <p className='desc'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, delectus?</p>
         </div>
         <div className='underline'></div>
-        <button className='btn' onClick={handleClick}>View More</button>
+        <button className='btn' onClick='/App'>View More</button>
+      </div>
+
+      <div className='box'>
+        <img className='image' src={Back} alt="background" />
+        <div className='desc-box'>
+          <h3 className='title'>Albert hall</h3>
+          <div className='underline'></div>
+          <p className='desc'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, delectus?</p>
+        </div>
+        <div className='underline'></div>
+        <button className='btn' onClick='/App'>View More</button>
+      </div>
+      <div className='box'>
+        <img className='image' src={Back} alt="background" />
+        <div className='desc-box'>
+          <h3 className='title'>Albert hall</h3>
+          <div className='underline'></div>
+          <p className='desc'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, delectus?</p>
+        </div>
+        <div className='underline'></div>
+        <button className='btn' onClick='/App'>View More</button>
+      </div>
+      <div className='box'>
+        <img className='image' src={Back} alt="background" />
+        <div className='desc-box'>
+          <h3 className='title'>Albert hall</h3>
+          <div className='underline'></div>
+          <p className='desc'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, delectus?</p>
+        </div>
+        <div className='underline'></div>
+        <button className='btn' onClick='/App'>View More</button>
+      </div>
+      <div className='box'>
+        <img className='image' src={Back} alt="background" />
+        <div className='desc-box'>
+          <h3 className='title'>Albert hall</h3>
+          <div className='underline'></div>
+          <p className='desc'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, delectus?</p>
+        </div>
+        <div className='underline'></div>
+        <button className='btn' onClick='/App'>View More</button>
+      </div>
     </div>
+
   )
 }
 
