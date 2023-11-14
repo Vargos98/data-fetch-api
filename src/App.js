9d1711688740ee595744e3685a94095929be0712
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-11-14 11:36:54 +0530

    Added fetched data from the Api from Jikan open source website

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 
+import { useEffect } from 'react';
 import Card from './Card/Card';
 import { Navbar } from './Navbar/Navbar';
 
 function App() {
+
+  const getData = async()=>{
+    const res = await fetch(`https://api.jikan.moe/v4/anime?q=naruto &sfw`);
+    const resData = await res.json();
+
+    console.log(resData);
+  }
+
+  useEffect(()=>{
+    getData();
+
+  },[])
 
   
   return (
